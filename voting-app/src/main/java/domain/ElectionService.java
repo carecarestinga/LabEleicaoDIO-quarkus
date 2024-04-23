368064df7dcf82da69696edde7d3006cccd0eb24
carecarestinga <carecarestinga@gmail.com>
2024-04-22 21:28:55 -0300

    adicionando as features de eventos reativos e cache na api de votos

---
+++ voting-app/src/main/java/domain/ElectionService.java
@@ -0,0 +1,29 @@
+package domain;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.util.List;
+
+/**
+ * Classe de serviço do dominio de voto
+ */
+@ApplicationScoped
+public class ElectionService {
+    private final ElectionRepository repository;
+
+    public ElectionService(ElectionRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Election> findAll() {
+        return repository.findAll();
+    }
+
+    public void vote(String electionId, String candidateId) {
+        repository.findById(electionId)
+                .candidates()
+                .stream()
+                .filter(candidate -> candidate.id().equals(candidateId))
+                .findFirst()
+                .ifPresent(candidate -> repository.vote(electionId, candidate));
+    }
+}
